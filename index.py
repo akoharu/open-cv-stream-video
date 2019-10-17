c40992dd456dc8792414cf4eb6d3b36e1999f94e
Asep Kohar <asep.kahar@gmail.com>
2019-10-17 09:07:24 +0700

    init

---
+++ index.py
@@ -0,0 +1,90 @@
+from flask import Response
+from flask import Flask
+from flask import render_template
+from imutils.video import VideoStream
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2.cv2 as cv2
+
+# initialize a flask framework
+app = Flask(__name__)
+
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful for multiple browsers/tabs
+# are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+
+# initialize the video stream and allow the camera sensor to start
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/video_feed")
+def video_feed():
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(generate(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+def cv_video():
+    # grab global references to the video stream, output frame, and lock variables
+    global vs, outputFrame, lock
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        with lock:
+            outputFrame = frame.copy()
+
+
+def generate():
+    # grab global references to the output frame and lock variables
+    global outputFrame, lock
+
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
+
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
+
+        # yield the output frame in the byte format
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+              bytearray(encodedImage) + b'\r\n')
+
+
+if __name__ == '__main__':
+    # construct the argument parser and parse command line arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", type=str, required=True,
+                    help="ip address of the device")
+    ap.add_argument("-o", "--port", type=int, required=True,
+                    help="ephemeral port number of the server (1024 to 65535)")
+    args = vars(ap.parse_args())
+    # start a thread that will perform motion detection
+    t = threading.Thread(target=cv_video)
+    t.daemon = True
+    t.start()
+    app.run(host=args["ip"], port=args["port"], debug=True,
+            threaded=True, use_reloader=True)
+
+# release the video stream pointer
+vs.stop()

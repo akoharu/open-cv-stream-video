ac4f1c0a6a09b2fd19b18b4e6f6db98b3f6a13a2
Asep Kohar <asep.kahar@gmail.com>
2019-10-17 09:24:36 +0700

    add time

--- index.py
+++ index.py
@@ -42,6 +42,12 @@ def cv_video():
     while True:
         frame = vs.read()
         frame = imutils.resize(frame, width=400)
+        # grab the current timestamp and draw it on the frame
+        timestamp = datetime.datetime.now()
+        cv2.putText(frame, timestamp.strftime(
+            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
         with lock:
             outputFrame = frame.copy()
 
